b708eb6d482ee86a61076fc73fefb6eee0d976ef
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-05-23 21:18:48 -0400

    updated folder name from youtube-search-app to well-buddies

---
+++ well-buddies/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MainTabBar from './navigation/main_tab_bar';
+import { LogBox } from "react-native";
+
+// disable really annoying in app warnings
+LogBox.ignoreAllLogs();
+
+const App = (props) => {
+  //<Text>hello world!</Text>
+  return <MainTabBar />;
+};
+
+
+export default App;
+/* 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ */
