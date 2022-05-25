ec1d425001a9c1c238bfe7bef83b9632f8691aee
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-05-25 13:38:00 -0400

    view calendar events

--- well-buddies/src/components/calendar.js
+++ well-buddies/src/components/calendar.js
@@ -7,23 +7,24 @@ import {
   GoogleSigninButton,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
-import axios from 'axios';
+import { getPrimaryCalendar, getCalendarEvents } from '../services/google-cal-api';
 
 function Calendar() {
   const [loggedIn, setloggedIn] = useState(false);
   const [userInfo, setuserInfo] = useState([]);
   const [accessToken, setAccessToken] = useState('');
+  const [calendarEvents, setEvents] = useState([]);
   // const CALENDER_CLIENT_ID = '301956188397-pbr5kqsk2mina10bq4i67uvb92v8p6l3.apps.googleusercontent.com';
   // const FIREBASE_CLIENT_ID = '529502837326-a50ihbbtkesh1qag5r1ce3doccd9jffl.apps.googleusercontent.com';
   const CLIENT_ID_IOS = '301956188397-rtuq8kgubluo5ismq4g9pq4cn9bag7ul.apps.googleusercontent.com';
-  const API_URL = 'https://www.googleapis.com/calendar/v3';
 
   useEffect(() => {
     GoogleSignin.configure({
       iosClientId: CLIENT_ID_IOS,
       scopes: ['https://www.googleapis.com/auth/calendar'],
     });
-  }, [loggedIn]);
+    getCalendars();
+  }, [loggedIn, accessToken]);
 
   const signIn = async () => {
     try {
@@ -31,8 +32,6 @@ function Calendar() {
       const userObject = await GoogleSignin.signIn();
       const token = await GoogleSignin.getTokens();
       setAccessToken(token.accessToken);
-      // console.log('token after signing in', token);
-      // console.log('access token', token?.accessToken);
       setloggedIn(true);
       setuserInfo(userObject);
       addScope();
@@ -70,23 +69,30 @@ function Calendar() {
   console.log('userinfo', userInfo.user);
 
   const addScope = async () => {
-    const scope = await GoogleSignin.addScopes({ scopes: ['https://www.googleapis.com/auth/calendar'] });
+    const scope = await GoogleSignin.addScopes({ scopes: ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events'] });
     console.log('add scpope', scope);
   };
 
-  // const getCalendars = async () => {
-  //   if (loggedIn) {
-  //     console.log('logged');
-  //     try {
-  //       const { tokens } = await GoogleSignin.getTokens();
-  //       console.log('tokens', tokens);
-  //       const calendars = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/q4hprhq5gnjkrpqsljr3b0541o@group.calendar.google.com/?access_token=${tokens.accessToken}`);
-  //       console.log("calendars", calendars);
-  //     } catch (error) {
-  //       console.log('get error', error);
-  //     }
-  //   }
-  // }
+  const getCalendars = async () => {
+    if (accessToken) {
+      try {
+        const primaryCal = await getPrimaryCalendar(accessToken);
+        console.log('primary calendar', primaryCal);
+        const events = await getCalendarEvents(accessToken);
+        // const calEvents = events.items.map((event) => <Text>{event.summary}</Text>);
+        setEvents(events);
+        console.log('events', events);
+      } catch (error) {
+        console.log('get cal error', error);
+      }
+    }
+  };
+
+  let currentEvents = null;
+
+  if (calendarEvents.items) {
+    currentEvents = calendarEvents.items.map((event) => { return <Text>{event.summary}</Text>; });
+  }
 
   return (
     <View style={styles.container}>
@@ -100,7 +106,8 @@ function Calendar() {
           {' '}
           signed in
         </Text>
-      ) : <Text>not signed in</Text> }
+      ) : <Text>not signed in</Text>}
+      {currentEvents}
       <GoogleSigninButton
         style={{ width: 192, height: 48 }}
         size={GoogleSigninButton.Size.Wide}
--- well-buddies/src/services/google-cal-api.js
+++ well-buddies/src/services/google-cal-api.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/calendar/v3/calendars/';
 const API_KEY = 'AIzaSyCvvnoQG--w8CR6pVkOMxqm-cid2ZNDZxY';
-const CALENDAR_ID = 'q4hprhq5gnjkrpqsljr3b0541o@group.calendar.google.com';
+// const CALENDAR_ID = 'q4hprhq5gnjkrpqsljr3b0541o@group.calendar.google.com';
 
-const params = {
-  key: API_KEY,
-  maxResults: 15,
-};
+// const params = {
+//   key: API_KEY,
+//   maxResults: 15,
+// };
 
-export function getCalendars(accessToken) {
+export function getPrimaryCalendar(accessToken) {
   return new Promise((resolve, reject) => {
-    axios.get(`${API_URL}${CALENDAR_ID}/events`, { params })
+    axios.get(`${API_URL}primary?access_token=${accessToken}`)
       .then((response) => {
         resolve(response.data);
       })
@@ -22,9 +22,9 @@ export function getCalendars(accessToken) {
   });
 }
 
-export function getCalendar() {
+export function getCalendarEvents(accessToken) {
   return new Promise((resolve, reject) => {
-    axios.get(`${API_URL}${CALENDAR_ID}/events`, { params })
+    axios.get(`${API_URL}primary/events?access_token=${accessToken}`)
       .then((response) => {
         resolve(response.data);
       })
