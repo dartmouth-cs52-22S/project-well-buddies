77ef448338b9dff71e8654b908898ff170bf01a4
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-05-25 13:01:01 -0400

    api beginning

---
+++ well-buddies/src/services/google-cal-api.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/calendar/v3/calendars/';
+const API_KEY = 'AIzaSyCvvnoQG--w8CR6pVkOMxqm-cid2ZNDZxY';
+const CALENDAR_ID = 'q4hprhq5gnjkrpqsljr3b0541o@group.calendar.google.com';
+
+const params = {
+  key: API_KEY,
+  maxResults: 15,
+};
+
+export function getCalendars(accessToken) {
+  return new Promise((resolve, reject) => {
+    axios.get(`${API_URL}${CALENDAR_ID}/events`, { params })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(`calendar api error: ${error}`);
+        reject(error);
+      });
+  });
+}
+
+export function getCalendar() {
+  return new Promise((resolve, reject) => {
+    axios.get(`${API_URL}${CALENDAR_ID}/events`, { params })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(`calendar api error: ${error}`);
+        reject(error);
+      });
+  });
+}
