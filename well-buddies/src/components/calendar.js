b4b9660c4f6e8d6d4cd22845a648ee0e0320ed00
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-05-25 01:03:11 -0400

    added oauth for calendar YAY

--- well-buddies/src/components/calendar.js
+++ well-buddies/src/components/calendar.js
@@ -5,18 +5,20 @@ import {
   GoogleSigninButton,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
+import axios from 'axios';
 
 const Calendar = () => {
 
+  let tokenClient;
   const [loggedIn, setloggedIn] = useState(false);
   const [userInfo, setuserInfo] = useState([]);
-  // const CLIENT_ID = "301956188397-pbr5kqsk2mina10bq4i67uvb92v8p6l3.apps.googleusercontent.com";
+  const CALENDER_CLIENT_ID = "301956188397-pbr5kqsk2mina10bq4i67uvb92v8p6l3.apps.googleusercontent.com";
   const FIREBASE_CLIENT_ID = "529502837326-a50ihbbtkesh1qag5r1ce3doccd9jffl.apps.googleusercontent.com";
-
+  const CLIENT_ID_IOS = "301956188397-rtuq8kgubluo5ismq4g9pq4cn9bag7ul.apps.googleusercontent.com";
   useEffect(() => {
     GoogleSignin.configure({
-      webClientId: FIREBASE_CLIENT_ID,
-      offlineAccess: false,
+      iosClientId: CLIENT_ID_IOS,
+      scopes: ['https://www.googleapis.com/auth/calendar'],
     });
   }, []);
 
@@ -29,6 +31,7 @@ const Calendar = () => {
       console.log('username', userObject.user.name);
       setloggedIn(true);
       setuserInfo(userObject);
+      getCalendars();
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
@@ -49,6 +52,7 @@ const Calendar = () => {
     }
   };
 
+
   signOut = async () => {
     try {
       await GoogleSignin.revokeAccess();
@@ -60,19 +64,32 @@ const Calendar = () => {
     }
   };
 
+  // signInCal = async () => {
+  //   ApiCalendar.handleAuth();
+  // };
+
   console.log('userinfo', userInfo.user);
+
+  const API_URL = "https://www.googleapis.com/calendar/v3";
+
+  const getCalendars = async () => {
+    const addScope = await GoogleSignin.addScopes({ scopes: ['https://www.googleapis.com/auth/calendar'] });
+    console.log('add scpope', addScope);
+    const calendars = await axios.get(`${API_URL}/calendars/q4hprhq5gnjkrpqsljr3b0541o@group.calendar.google.com`);
+    console.log("calendars", calendars);
+  }
 
     return (
       <View style={styles.container}>
         <Text>
           Calendar
         </Text>
-        {loggedIn ? <Text>{userInfo.user.name} signed in</Text> :  <Text>not signed in</Text> }
+        {loggedIn ? <Text> {userInfo?.user?.name } signed in</Text> :  <Text>not signed in</Text> }
          <GoogleSigninButton
                 style={{width: 192, height: 48}}
                 size={GoogleSigninButton.Size.Wide}
                 color={GoogleSigninButton.Color.Dark}
-                onPress={this.signIn}
+                onPress={signIn}
               />
       </View>
     );
