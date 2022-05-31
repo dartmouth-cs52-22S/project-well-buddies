7b8da29dbdb91989b327f7db716070f60ade149e
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-05-31 17:49:55 -0400

    small edits to get app back running (styling updates, etc.)

--- well-buddies/src/components/calendar/event.js
+++ well-buddies/src/components/calendar/event.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import {
-  StyleSheet,View, Text,
+  StyleSheet, View, Text,
 } from 'react-native';
 import { Card } from 'react-native-elements';
-import CustomText from '../custom/custom_text';
+// import CustomText from '../custom/custom_text';
+
 import Moment from 'moment';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
 
-
-function EventDetail(props) {
-  // example of destructuring, the below is equivalent to props.route.params.video
-  const { route } = props;
-  const { event } = route.params;
-
-  return (
-    <Card>
-      <CustomText><Text style={styles.title}>{event.summary}</Text></CustomText>
-      <CustomText><Text style={styles.date}>{parseDay(event.start.dateTime)},{parseDate(event.start.dateTime)} </Text></CustomText>
-      <CustomText>
-      <Text style={styles.time}>
-              {parseStartTime(event.start.dateTime)}
-              {' '}
-              -
-              {' '}
-              {parseEndTime(event.end.dateTime)}
-      </Text>
-      </CustomText>
-      <CustomText><Text style={styles.email}><Ionicons name={'calendar-o'} size={20} color='#818589'/>  {event.organizer.email}</Text></CustomText>
-    </Card>
-  );
-}
 function parseStartTime(dateTime) {
   Moment.locale('en');
   return Moment(dateTime).format('h:mm');
@@ -47,29 +25,58 @@ function parseDate(dateTime) {
   Moment.locale('en');
   return Moment(dateTime).format(' MMMM D, YYYY');
 }
+
+function EventDetail(props) {
+  // example of destructuring, the below is equivalent to props.route.params.video
+  const { route } = props;
+  const { event } = route.params;
+
+  return (
+    <Card>
+      <Text style={styles.title}>{event.summary}</Text>
+      <Text style={styles.date}>
+        {parseDay(event.start.dateTime)}
+        ,
+        {parseDate(event.start.dateTime)}
+        {' '}
+      </Text>
+      <Text style={styles.time}>
+        {parseStartTime(event.start.dateTime)}
+        {' '}
+        -
+        {' '}
+        {parseEndTime(event.end.dateTime)}
+      </Text>
+      <Text style={styles.email}>
+        <Ionicons name="calendar-o" size={20} color="#818589" />
+        {' '}
+        {event.organizer.email}
+      </Text>
+    </Card>
+  );
+}
+
 const styles = StyleSheet.create({
   title: {
     fontWeight: '600',
     fontSize: 28,
-    color: '#45587c'
+    color: '#45587c',
   },
   time: {
     fontWeight: '600',
     fontSize: 15,
-    color: '#45587c'
+    color: '#45587c',
   },
   date: {
     fontWeight: '600',
     fontSize: 15,
-    color: '#45587c'
+    color: '#45587c',
   },
   email: {
     fontWeight: '600',
-    color: '#45587c'
+    color: '#45587c',
   },
 
 });
-
-
 
 export default EventDetail;
